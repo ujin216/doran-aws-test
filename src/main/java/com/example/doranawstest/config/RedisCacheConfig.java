26161d6dffb4fba6b03ea3a4a2a74bf0b2067e83
ujin216 <ujin3446@gmail.com>
2025-08-25 23:26:13 +0900

    commit

---
+++ src/main/java/com/example/doranawstest/config/RedisCacheConfig.java
@@ -0,0 +1,41 @@
+package com.example.doranawstest.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+    private static final int DEFAULT_TTL = 1;
+
+    @Bean
+    public RedisCacheManager redisCacheManager(
+            RedisConnectionFactory redisConnectionFactory,
+            Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer
+    ) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .computePrefixWith(cacheName -> cacheName + ":")
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+                )
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer)
+                )
+                .entryTtl(Duration.ofMinutes(DEFAULT_TTL));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
